dfb751346a717858ac5cfe40d6165f7506b66f54
alex1779 <alex1779@gmail.com>
2022-05-13 14:15:55 -0300

    first commit

---
+++ alignedFaceHorizontal_webcam.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 13 11:51:12 2022
+
+@author: Alejandro Maggioni
+"""
+
+import cv2
+import mediapipe as mp
+import imutils
+from drawing import alignedFaceHorizontal
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_face_mesh = mp.solutions.face_mesh
+
+
+def main():
+
+    cap = cv2.VideoCapture(0)
+    with mp_face_mesh.FaceMesh(
+            max_num_faces=1,
+            refine_landmarks=True,
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5) as face_mesh:
+        while cap.isOpened():
+            success, image = cap.read()
+            if not success:
+                print("Ignoring empty camera frame.")
+                break
+
+            # To improve performance, optionally mark the image as not writeable to
+            # pass by reference.
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            height, width, _ = image.shape
+            results = face_mesh.process(image)
+
+            # Draw the face mesh annotations on the image.
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            if results.multi_face_landmarks:
+                for face_landmarks in results.multi_face_landmarks:
+
+                    image = alignedFaceHorizontal(
+                        image=image,
+                        face_landmarks=face_landmarks,
+                    )
+
+            image = imutils.resize(image, height=720)
+            cv2.imshow('MediaPipe Face Mesh Tesselation', image)
+            if cv2.waitKey(5) & 0xFF == 27:
+                cap.release()
+                cv2.destroyAllWindows()
+                
+                break
+
+    cv2.destroyAllWindows()
+    cap.release()
+
+
+if __name__ == "__main__":
+    main()
